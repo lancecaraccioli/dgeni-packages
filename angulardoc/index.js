8e58561ced35a440dbd80afdc427dd889da560ee
Lance Caraccioli <lance.caraccioli@gmail.com>
2014-10-02 11:32:50 -0500

    remove deployments from new 'angulardoc' dgeni-package

--- angulardoc/index.js
+++ angulardoc/index.js
@@ -13,62 +13,29 @@ module.exports = new Package('angulardoc', [
   require('dgeni-packages/examples')
 ])
 
-
 .factory(require('./services/errorNamespaceMap'))
-.factory(require('./services/getMinerrInfo'))
-.factory(require('./services/getVersion'))
-.factory(require('./services/gitData'))
-
-.factory(require('./services/deployments/debug'))
-.factory(require('./services/deployments/default'))
-.factory(require('./services/deployments/jquery'))
-.factory(require('./services/deployments/production'))
 
 .factory(require('./inline-tag-defs/type'))
 
-
+.processor(require('./processors/branding-data'))
 .processor(require('./processors/error-docs'))
 .processor(require('./processors/index-page'))
 .processor(require('./processors/keywords'))
 .processor(require('./processors/pages-data'))
 .processor(require('./processors/versions-data'))
-.processor(require('./processors/branding-data'))
-
-
-.config(function(dgeni, log, readFilesProcessor, writeFilesProcessor) {
-
-  dgeni.stopOnValidationError = true;
-  dgeni.stopOnProcessingError = true;
-
-  log.level = 'info';
-
-  readFilesProcessor.basePath = path.resolve(__dirname,'../..');
-  readFilesProcessor.sourceFiles = [
-    { include: 'src/**/*.js', basePath: 'src' },
-    { include: 'docs/content/**/*.ngdoc', basePath: 'docs/content' }
-  ];
-
-  writeFilesProcessor.outputFolder = 'build/docs';
-
-})
-
 
 .config(function(parseTagsProcessor) {
   parseTagsProcessor.tagDefinitions.push(require('./tag-defs/tutorial-step'));
   parseTagsProcessor.tagDefinitions.push(require('./tag-defs/sortOrder'));
 })
 
-
 .config(function(inlineTagProcessor, typeInlineTagDef) {
   inlineTagProcessor.inlineTagDefinitions.push(typeInlineTagDef);
 })
 
-
-.config(function(templateFinder, renderDocsProcessor, gitData) {
+.config(function(templateFinder) {
   templateFinder.templateFolders.unshift(path.resolve(packagePath, 'templates'));
-  renderDocsProcessor.extraData.git = gitData;
 })
-
 
 .config(function(computePathsProcessor, computeIdsProcessor) {
 
@@ -130,32 +97,4 @@ module.exports = new Package('angulardoc', [
     getId: function(doc) { return 'error:' + doc.name; },
     getAliases: function(doc) { return [doc.id]; }
   });
-})
-
-
-.config(function(
-  generateIndexPagesProcessor,
-  generateProtractorTestsProcessor,
-  generateExamplesProcessor,
-  debugDeployment, defaultDeployment,
-  jqueryDeployment, productionDeployment) {
-
-  generateIndexPagesProcessor.deployments = [
-    debugDeployment,
-    defaultDeployment,
-    jqueryDeployment,
-    productionDeployment
-  ];
-
-  generateProtractorTestsProcessor.deployments = [
-    defaultDeployment,
-    jqueryDeployment
-  ];
-
-  generateExamplesProcessor.deployments = [
-    debugDeployment,
-    defaultDeployment,
-    jqueryDeployment,
-    productionDeployment
-  ];
 });
